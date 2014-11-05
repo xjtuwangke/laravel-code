8f0d9a5b078333b5fb08f96ecc240efb6eaadee5
wangke <wangke85@gmail.com>
2014-11-05 22:47:30 +0800

    二维码可以设置png格式的logo

---
+++ src/config/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 14/11/5
+ * Time: 22:45
+ */
+
+
+return array(
+    'logo' => null ,
+);
--- src/controllers/CodeController.php
+++ src/controllers/CodeController.php
@@ -64,14 +64,36 @@ class CodeController extends \Controller{
 
     public function qrcode( $code ){
         $qrCode = new QrCode();
-        $qrCode->setText( $code )
+        $QR = $qrCode->setText( $code )
             ->setSize( 300 )
             ->setPadding( 10 )
             ->setImageType( QrCode::IMAGE_TYPE_JPEG )
             ->setErrorCorrection( QrCode::LEVEL_HIGH )
-            ->render();
+            ->getImage();
+
         $response = \Response::make( '' , 200 );
         $response->header( 'Content-Type' , 'image/jpeg' , true );
+        header('Content-type: image/png');
+
+        if( ( $logo = \Config::get( 'laravel-code::config.logo' ) ) && \File::exists( $logo ) ){
+            $QR_width = imagesx($QR);
+            $QR_height = imagesy($QR);
+            $im = imagecreatetruecolor( $QR_width , $QR_height );
+            imagecopy( $im , $QR , 0 , 0 , 0 , 0 , $QR_width , $QR_height );
+            imagedestroy( $QR );
+            $logo = imagecreatefromstring( file_get_contents( $logo ) );
+            $logo_width = imagesx($logo);
+            $logo_height = imagesy($logo);
+            $logo_qr_width = $QR_width / 5;
+            $scale = $logo_width / $logo_qr_width;
+            $logo_qr_height = $logo_height / $scale;
+            $from_width = ($QR_width - $logo_qr_width) / 2;
+            imagecopyresampled($im, $logo, $from_width, $from_width, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
+            imagepng( $im );
+        }
+        else{
+            imagepng( $QR );
+        }
         return $response;
     }
 } 
